50c8da3e70c0fd81829a752f68dae716b57e0047
CristhianMotoche <cristhian.motoche@gmail.com>
2019-04-17 14:03:56 -0500

    Add tracks list page

---
+++ templates/public/js/tracks.js
@@ -0,0 +1,24 @@
+/**
+ * Fetch tracks and append them to the table
+ *
+ * ****************************
+ * Please change 'json/tracks.json'
+ * with your service endpoint below
+ * ****************************
+ */
+fetch('json/tracks.json')
+  .then(response => response.json())
+  .then(tracks => {
+    let rows = tracks.map(element => createTracksTemplate(element));
+    let table = $("#tracks tbody");
+    table.append(rows);
+  });
+
+/**
+ * Find the template tag and populate it with the data
+ * @param order
+ */
+function createTracksTemplate(track) {
+  let template = $("#track-item-template")[0].innerHTML;
+  return Mustache.render(template, track);
+}
