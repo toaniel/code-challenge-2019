6531d93f2b2b8780bff8b21f407b03710bf48b76
Daniel Holguin <faustodanielholguin@gmail.com>
2019-05-18 15:15:59 -0500

    (node:83152) DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect. Example app listening on port 3002!

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,70 @@
 const express = require('express')
 const app = express()
 const port = 3002
-const cors = require('cors')
 
+/* Give cors permissions */
+const cors = require('cors')
 app.use (cors())
+
+/* Database */
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+
+mongo.connect(url, (err, client) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+const db = client.db('banana_running_db')
+const collection_tracks = db.collection('tracks')
+const collection_workouts = db.collection('workouts')
+
+collection_tracks.insertOne( {
+		"id": 1,
+		"name": "Running around La Carolina",
+		"distance": 2000,
+		"time": 3600,
+		"date": "2019-01-01T23:00:00.000Z"
+	  });
+collection_tracks.insertOne( 	  {
+		"id": 2,
+		"name": "Running around La Alameda",
+		"distance": 5000,
+		"time": 1800,
+		"date": "2019-01-03T23:00:00.000Z"
+	  });
+collection_tracks.insertOne( 	  {
+		"id": 3,
+		"name": "Running around Parque Metropolitano",
+		"distance": 3000,
+		"time": 4800,
+		"date": "2019-01-04T23:00:00.000Z"
+	  });
+
+collection_workouts.insertOne( {
+		"id": 1,
+		"name": "Running around La Carolina",
+		"distance": 2000,
+		"velocity": 0.5,
+		"time": 60,
+		"date": "2019-01-04T23:00:00.000Z",
+		"points": [
+		{
+		  "lat": -0.1821626,
+		  "long": -78.4857697
+		}, {
+		  "lat": -0.1834496,
+		  "long": -78.4875187
+		}, {
+		  "lat": -0.1817756,
+		  "long": -78.4869717
+		}
+		]
+	  });
+})
+
+/* end point handlers */
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/distances', function(req, res) {
